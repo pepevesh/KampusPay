88759ff68d93c85fa8e522b15f99612f8cd42532
hydroxy42 <farhan2262003@gmail.com>
2024-11-30 17:00:25 +0530

    logo added

--- frontend/src/app/manifest.js
+++ frontend/src/app/manifest.js
@@ -5,14 +5,9 @@ export default function manifest() {
       description: 'Digital Campus Payment and Budgeting Application',
       start_url: '/',
       display: 'standalone',
-      background_color: '#ffffff',
+      background_color: '#7F3DFF',
       theme_color: '#000000',
       icons: [
-        {
-          src: '/web-app-manifest-144x144.png',
-          sizes: '144x144',
-          type: 'image/png',
-        },
         {
           src: '/web-app-manifest-512x512.png',
           sizes: '512x512',
