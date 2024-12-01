b6d1dbfcd3009aefc6730595e99838ab0681e04e
Bhargavi-612 <bhargavi.acharya2018@gmail.com>
2024-12-01 08:08:04 +0530

    set limit

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -225,3 +225,22 @@ exports.getDailySpending = async (req, res) => {
     res.status(500).json({ message: 'Error fetching daily spending', error: error.message || error });
   }
 };
+
+exports.updateLimit = async (req, res) => {
+    try {
+        const { userId, newLimit } = req.body;
+
+        // Find the user
+        const user = await User.findOne({ userId });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        user.dailyLimit = newLimit; 
+        await user.save();
+
+        res.status(200).json({ message: 'Limit updated successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error', error: error.message });
+    }
+};
